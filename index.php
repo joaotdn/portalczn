b8ab9a7712f2d67052bbcb1908f5ffd12d6cd17f
Joao Teodoro <joaotdn@gmail.com>
2014-07-03 16:29:17 -0300

    Início do projeto

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html class="no-js off-canvas-wrap" lang="pt-br" data-offcanvas>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portal CZN | O Sertão em Notícias</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.min.js"></script>
+  </head>
+  <body class="inner-wrap">
+
+  <!-- Off Canvas Menu Left -->
+  <aside class="left-off-canvas-menu">
+      <ul class="off-canvas-list font-header">
+        <li><label>Editorias</label></li>
+        <li><a href="#">Cajazeiras</a></li>
+        <li><a href="#">Policial</a></li>
+        <li><a href="#">Política</a></li>
+        <li><a href="#">Brasil</a></li>
+        <li><a href="#">Paraíba</a></li>
+        <li><a href="#">Mundo</a></li>
+      </ul>
+  </aside>
+
+  <!-- Off Canvas Menu Right -->
+  <aside class="right-off-canvas-menu">
+      <ul class="off-canvas-list font-header">
+        <li><label>Seguir</label></li>
+        <li><a href="#" title="Seguir no Twitter"><span class="icon-twitter"></span> Twitter</a></li>
+        <li><a href="#" title="Seguir no Facebook"><span class="icon-facebook3"></span> Facebook</a></li>
+        <li><label>Fale conosco</label></li>
+        <li><a href="#"><span class="icon-mail"></span> Mande um email</a></li>
+        <li><a href="#"><span class="icon-megaphone"></span> Anuncie no site</a></li>
+      </ul>
+  </aside>
+
+  <!-- close the off-canvas menu -->
+  <a class="exit-off-canvas"></a>
+
+    <section class="small-16 top-bar" data-topbar>
+      <div class="row">
+        <nav class="top-bar-section">
+          <a class="left-off-canvas-toggle left show-for-small-only" href="#"><span class="icon-menu"></span></a>
+          <a class="right-off-canvas-toggle right show-for-small-only" href="#"><span class="icon-share"></span></a>
+
+          <ul class="left text-up font-header">
+            <li class="has-dropdown">
+              <a href="#" title="" class="active">Editorias</a>
+              <ul class="dropdown">
+                <li><a href="#">Cajazeiras</a></li>
+                <li><a href="#">Policial</a></li>
+                <li><a href="#">Política</a></li>
+                <li><a href="#">Brasil</a></li>
+                <li><a href="#">Paraíba</a></li>
+                <li><a href="#">Mundo</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Classificados</a></li>
+            <li><a href="#">Vídeos</a></li>
+          </ul>
+
+          <ul class="right">
+            <li><a href="#" title="Seguir no Facebook" data-tooltip data-options="disable_for_touch:true" class="has-tip tip-bottom radius round"><span class="icon-facebook3"></span></a></li>
+            <li><a href="#" title="Seguir no Twitter" data-tooltip data-options="disable_for_touch:true" class="has-tip tip-bottom radius round"><span class="icon-twitter"></span></a></li>
+            <li><a href="#" title="Anunciar no site" data-tooltip data-options="disable_for_touch:true" class="has-tip tip-bottom radius round"><span class="icon-megaphone"></span></a></li>
+            <li><a href="#" title="Contato" data-tooltip data-options="disable_for_touch:true" class="has-tip tip-bottom radius round"><span class="icon-mail"></span></a></li>
+          </ul>
+        </nav>
+
+      </div><!-- //row -->
+    </section><!-- //top-bar -->
+
+    <section id="header" class="small-16">
+      <div class="row">
+        <figure class="large-4 small-16 columns">
+          <a href="#" title="Página principal" class="display-block icon-logo show-for-medium-up"></a>
+          <a href="#" title="Página principal" class="display-block icon-logo-small hide-for-medium-up"></a>
+        </figure>
+
+        <figure class="large-10 small-16 columns ads-top show-for-medium-up">
+          <a href="#" class="display-block text-right medium-only-text-center">
+            <img src="media/ads1.png" alt="">
+          </a>
+        </figure>
+      </div><!-- //row -->
+    </section><!-- //header -->
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="scripts.js"></script>
+  </body>
+</html>
